59c87883b802ce9db67916803e777a83635897fb
foconer <frederick.oconer@consensuscorp.com>
2018-04-12 15:06:08 -0600

    test

---
+++ rest-ws-gradle-app/src/main/java/test/project/app/MainApp.java
@@ -0,0 +1,9 @@
+package test.project.app;
+
+import test.project.model.User;
+
+public class MainApp {
+    public static void main(String... args) {
+        System.out.println(User.builder().build());
+    }
+}
---
+++ rest-ws-gradle-model/src/main/java/test/project/model/User.java
@@ -0,0 +1,9 @@
+package test.project.model;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class User {
+}
